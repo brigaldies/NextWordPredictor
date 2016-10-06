6ad87977cd2c2d26cabb395843489091f50b294c
Bertrand Rigaldies <brigaldies@gmail.com>
2016-10-05 21:51:58 -0400

    Slides deck (Cont'd)

--- slides.md
+++ slides.md
@@ -20,9 +20,9 @@ height: 900
 
 
 
-* An N-grams-based probabilistic [model](#/model) to predict the next most likely words in an english sentence (See example of R output below)
-* Using Maximum Likelihood Estimates (MLE), and [Stupid Backoff Algorithm](#/predictor)-based weighted probabilities;
-* Achieving about 25% prediction [success rate](#/performance);
+* An N-grams-based probabilistic [model](#/model) to predict the next most likely words in an english sentence (See example of R output below);
+* Using Maximum Likelihood Estimates (MLE), and [Stupid Backoff Algorithm](#/predictor)-based prediction;
+* Achieving about 55% prediction [success rate](#/performance);
 * Test drive it [here](#/shinyapp).
 
 
@@ -41,37 +41,92 @@ predictWithStupidBackoff2(model = model, sentence = 'as far as I can', matches_c
 5:        because      as i can because           0.02519685 -2.7647455
 ```
 
-1. The Predictive Model
+1. The Probabilistic Model
 ========================================================
 id: model
+class: small-code
 
-* English Corpus f
-* Training dataset
-* N-grams
+* English Corpus from [HC Corpora](www.corpora.heliohost.org).
+* 10% of the English Corpus used as training dataset.
+* 5 N-grams tables built from the training dataset, N = 1 to 5.
+* Each N-gram table is structured as follows:
 
-$P_{PU}(w_n \ldots w_1) = \frac{Count(w_n \ldots w_1)}{Total Number of N-Grams}$
+Column | Description
+------------- | -------------
+**gram** | The gram as a string of one ($N=1$) or more ($N\gt2$) words.
+**count** | The number of times the gram is used in the Corpus training dataset.
+**lowergram** | The preceding lower-order gram when $N\gt2$, or "lowergram" (e.g., "in the" in gram "in the world").
+**last_word_in_gram** | The last word in the gram (e.g., "world" in gram "in the world").
+**logprob** | Logarithm of the gram's **Maximum Likelihood Estimation** (MLE).
+**logpercent** | Logarithm of the gram's **Percent Use** (PU) based on the gram's count.
 
-$P_{MLE}(w_n \ldots w_1) = P(w_n|w_{n-1} \ldots w_1) = \frac{Count(w_n \ldots w_1)}{Count(w_{n-1} \ldots w_1)}$
+**Maximum Likelihood Estimation**: $P_{MLE}(w_n \ldots w_1) = P(w_n|w_{n-1} \ldots w_1) = \frac{Count(w_n \ldots w_1)}{Count(w_{n-1} \ldots w_1)}$
 
+**Percent Use**: $P_{PU}(w_n \ldots w_1) = \frac{Count(w_n \ldots w_1)}{Total Number of N-Grams}$
 
 2. The Stupid Backoff Algorithm
 ========================================================
 id: predictor
 
-![alt text](predictor.jpg)
+Reference: [Speech and Language Processing, Daniel Jurafsky & James H. martin, January 9, 2015, Chapter 4: N-Grams, section 4.5.1 The Web and Stupid Backoff](https://web.stanford.edu/~jurafsky/slp3/4.pdf)
 
-3. Quantitative Predictive Performance
+The basic flow is:
+
+1. Lookup the top 10 matches in the highest order N-gram table in the model, where lowergram = last (N-1) words in the input sentence;
+1. If less than 10 matches were found, **back off** and look up for additional matches in the (N-1)-gram table;
+1. At each backoff, *discount* the matches' MLE probabilities by applying a $\lambda$ weight. (e.g., $\lambda=0.4$): $P_{MLE(weighted)}(w_{1} \ldots w_{N-1}w_{match}) = \lambda^{(4 - (N-1))} * P_{MLE}(w_{1} \ldots w_{N-1}w_{match})$
+1. Repeat the previous step until 10 matches are found, using the unigrams table as the last lookup step.
+1. Return the top 10 matches (or predictions), sorted by $\lambda\$-weighted MLE.
+
+3. Quantitative Predictive Performance: ~55%
 ========================================================
 id: performance
+class: small-code
 
-* Test dataset
-* Test
-* Result
+* Sample 10% of the non-training data in the English Corpus to create a test dataset;
+* Build a bi, tri, quad, and pentagrams from the test dataset;
+* For each N-gram, build a `data.table` with the following columns: a) `gram`: The gram; b) `lowergram`: The gram's immediate lower-order gram; c) `word_to_predict`: the gram's last word
+* Sample 25,000 grams from each N-gram (N=2 to 5), and `rbind` the resulting samples into a 100,000-row table:
+
+```
+                    gram   lowergram word_to_predict
+1:        are we to make   are we to            make
+2: make better decisions make better       decisions
+3:          its too much     its too            much
+```
+* **Success Criteria**: For each `gram` in the test `data.table`, run the Stupid Backoff prediction on the `lowergram`, and if the resulting 10 predictions contain the gram's `word_to_predict`, then the test is a PASS, else the test is a FAIL.
+
+```r
+set.seed(10); testSample = sample_n(testData, 1)
+print(testSample)
+```
+
+```
+             gram lowergram word_to_predict
+1: are we to make are we to            make
+```
+
+```r
+predictWithStupidBackoff2(model = model, sentence = testSample$lowergram)
+```
+
+```
+    Predicted Word      From Gram Weighted Probability MLE (Log)
+ 1:           make are we to make         0.0615384615 -1.871802
+ 2:           play are we to play         0.0615384615 -1.871802
+ 3:             do       we to do         0.0177777778 -2.197225
+ 4:            the         to the         0.0050152354 -2.546403
+ 5:             be          to be         0.0037864492 -2.827454
+ 6:            get         to get         0.0016445112 -3.661440
+ 7:              a           to a         0.0012895389 -3.904598
+ 8:            see         to see         0.0012541580 -3.932419
+ 9:             go          to go         0.0009725078 -4.186760
+10:           have        to have         0.0009164106 -4.246174
+```
 
 4. My Shinyapps.io Application
 ========================================================
 id: shinyapp
-
 
 ![alt text](shinyappio.jpg)
 
--- test_model.R
+++ test_model.R
@@ -1,8 +1,25 @@
+source('config.R')
+source('utils.R')
+source('load_corpus.R')
+source('predictor.R')
+
+require(dplyr)
+
+# -----------------------------------------------------------------------------
+# Load the model under test
+# -----------------------------------------------------------------------------
+modelUnderTest = readRDS('model.rds')
+# Poke at the data
+head(modelUnderTest$unigrams[order(logprob, decreasing = TRUE)])
+
 # -----------------------------------------------------------------------------
 # Load and clean the test corpus
 # -----------------------------------------------------------------------------
 partitionRate = 10
-sampleRate = 1
+sampleRate = 5
+partitionCorpusFile(directory = g_corpus_directory_en, file_name = paste0('en_US.blogs.txt.partition_', sprintf('%.1f', 100-partitionRate)), sample_rate = sampleRate/100)
+partitionCorpusFile(directory = g_corpus_directory_en, file_name = paste0('en_US.news.txt.partition_', sprintf('%.1f', 100-partitionRate)), sample_rate = sampleRate/100)
+partitionCorpusFile(directory = g_corpus_directory_en, file_name = paste0('en_US.twitter.txt.partition_', sprintf('%.1f', 100-partitionRate)), sample_rate = sampleRate/100)
 # Check the file pattern
 corpusTestFilesPattern = paste0('^en_US(.)*partition_', sprintf('%.1f', 100-partitionRate), '.partition_', sprintf('%.1f', sampleRate), '$')
 dirSamples = DirSource(g_corpus_directory_en, pattern = corpusTestFilesPattern)
@@ -10,42 +27,245 @@ print(dirSamples$filelist)
 
 cTest = loadCorpus(directory = g_corpus_directory_en, files_pattern = corpusTestFilesPattern)
 cTest <- cleanCorpus(cTest)
+cTestPathName = paste0(g_corpus_directory_en, '\\test_cleaned_corpus_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
+saveRDS(object = cTest, file = cTestPathName)
+# cTest = readRDS(file = cTestPathName)
 
-# Create the test quadgrams
-ngramName = 'quadgram'
-message(paste('Get a', ngramName, 'tokenizer...'))
+# -----------------------------------------------------------------------------
+# Create the test bigrams
+# ~ 21% success
+# -----------------------------------------------------------------------------
+tokenizer2 = function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+testBigramsDtmPathName = paste0(g_corpus_directory_en, '\\test_bigrams_dtm_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
+execTime = system.time({
+    testBigramsDtm = DocumentTermMatrix(cTest, control = list(tokenize = tokenizer2))
+    saveRDS(object = testBigramsDtm, file = testBigramsDtmPathName)
+})
+message(paste('Test bigrams DTM built in', round(execTime["elapsed"], 2), "secs"))
+testBigramsCount = colSums(as.matrix(testBigramsDtm))
+testBigramsCount = testBigramsCount[testBigramsCount >= 2] # Remove single occurrences
+testBigramsGram = names(testBigramsCount)
+testBigramsLowerGram = sapply(testBigramsGram, function(gram) { getFirstNTokensFast(gram, 1)})
+testBigramsWordToPredict = sapply(testBigramsGram, function(gram) { getLastToken(gram) })
+testBigramsDT = data.table(gram = testBigramsGram, 
+                           lowergram = testBigramsLowerGram,
+                           word_to_predict = testBigramsWordToPredict)
+testBigramsDTPathName = paste0(g_corpus_directory_en, '/test_bigrams_DT_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
+saveRDS(object = testBigramsDT, file = testBigramsDTPathName)
+# testBigramsDT = readRDS(file = testBigramsDTPathName)
+dim(testBigramsDT)
+
+# Quick test the model
+set.seed(10)
+testSample = sample_n(testBigramsDT, 1)
+predictWithStupidBackoff2(model = modelUnderTest, sentence = testSample$lowergram, matches_count_max = 10)
+
+# Test the model's performance against the test bigrams
+execTime <- system.time({
+    set.seed(10)
+    testSize = 100000
+    remove(testData)
+    testData = sample_n(testBigramsDT, testSize)
+    testCount = dim(testData)[1]
+    testResults = mapply(function(lowergram, word_to_predict) {
+        word_to_predict %in% predictWithStupidBackoff2(model = modelUnderTest,
+                                                       sentence = lowergram)$`Predicted Word`
+    },
+    testData$lowergram,
+    testData$word_to_predict)
+    predictionmatchCount = sum(testResults)
+    message(paste('Prediction match count   :', predictionmatchCount, 'out of', testCount, '(', round(predictionmatchCount*100/testCount, 2), '%)'))
+})
+execTimeSeconds = execTime["elapsed"]
+execTimeMins = execTimeSeconds/60
+execTimeSecsAfterMins = execTimeSeconds %% 60
+message(paste('Test executed in', round(execTimeMins), 'mins, ', round(execTimeSecsAfterMins, 2), 'secs'))
+
+# -----------------------------------------------------------------------------
+# Create the test trigrams
+# ~ 52% success
+# -----------------------------------------------------------------------------
+tokenizer3 = function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+testTrigramsDtmPathName = paste0(g_corpus_directory_en, '\\test_trigrams_dtm_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
+execTime = system.time({
+    testTrigramsDtm = DocumentTermMatrix(cTest, control = list(tokenize = tokenizer3))
+    saveRDS(object = testTrigramsDtm, file = testTrigramsDtmPathName)
+})
+message(paste('Test trigrams DTM built in', round(execTime["elapsed"], 2), "secs"))
+testTrigramsCount = colSums(as.matrix(testTrigramsDtm))
+testTrigramsCount = testTrigramsCount[testTrigramsCount >= 2] # Remove single occurrences
+testTrigramsGram = names(testTrigramsCount)
+testTrigramsLowerGram = sapply(testTrigramsGram, function(gram) { getFirstNTokensFast(gram, 2)})
+testTrigramsWordToPredict = sapply(testTrigramsGram, function(gram) { getLastToken(gram) })
+testTrigramsDT = data.table(gram = testTrigramsGram, 
+                           lowergram = testTrigramsLowerGram,
+                           word_to_predict = testTrigramsWordToPredict)
+testTrigramsDTPathName = paste0(g_corpus_directory_en, '/test_trigrams_DT_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
+saveRDS(object = testTrigramsDT, file = testTrigramsDTPathName)
+# testTrigramsDT = readRDS(file = testTrigramsDTPathName)
+dim(testTrigramsDT)
+
+# Quick test the model
+set.seed(10)
+testSample = sample_n(testTrigramsDT, 1)
+predictWithStupidBackoff2(model = modelUnderTest, sentence = testSample$lowergram, matches_count_max = 10)
+
+# Test the model's performance against the test trigrams
+execTime <- system.time({
+    set.seed(10)
+    testSize = 100000
+    remove(testData)
+    testData = sample_n(testTrigramsDT, testSize)
+    testCount = dim(testData)[1]
+    testResults = mapply(function(lowergram, word_to_predict) {
+        word_to_predict %in% predictWithStupidBackoff2(model = modelUnderTest,
+                                                       sentence = lowergram)$`Predicted Word`
+    },
+    testData$lowergram,
+    testData$word_to_predict)
+    predictionmatchCount = sum(testResults)
+    message(paste('Prediction match count   :', predictionmatchCount, 'out of', testCount, '(', round(predictionmatchCount*100/testCount, 2), '%)'))
+})
+execTimeSeconds = execTime["elapsed"]
+execTimeMins = execTimeSeconds/60
+execTimeSecsAfterMins = execTimeSeconds %% 60
+message(paste('Test executed in', round(execTimeMins), 'mins, ', round(execTimeSecsAfterMins, 2), 'secs'))
+
+# -----------------------------------------------------------------------------
+# create the test quadgrams
+# ~ 74% success
+# -----------------------------------------------------------------------------
 tokenizer4 = function(x) NGramTokenizer(x, Weka_control(min = 4, max = 4))
-message(paste('Compute the DTM...'))
-testQuadgramsDtmPathName = paste0(g_corpus_directory_en, '/test_quadgrams_dtm_partition_', sprintf('%.1f', 100-partitionRate), '.partition_', sprintf('%.1f', sampleRate), '.rds')
+testQuadgramsDtmPathName = paste0(g_corpus_directory_en, '/test_quadgrams_dtm_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
 execTime = system.time({
     testQuadgramsDtm = DocumentTermMatrix(cTest, control = list(tokenize = tokenizer4))
     saveRDS(object = testQuadgramsDtm, file = testQuadgramsDtmPathName)
 })
-message(paste(ngramName, 'DTM built in', round(execTime["elapsed"], 2), "secs"))
-message(paste(ngramName, 'DTM size:', round(object.size(testQuadgramsDtm)/1024/2014, 2), 'MB'))
+message(paste('DTM built in', round(execTime["elapsed"], 2), "secs"))
 
-# Read the bigrams DTM if necessary
+# Read the quadgrams DTM if necessary
 testQuadgramsDtm = readRDS(file = testQuadgramsDtmPathName)
 
 # Get the list of quadgrams
 testQuadgramsCount = colSums(as.matrix(testQuadgramsDtm))
-# testQuadgramsCount = testQuadgramsCount[testQuadgramsCount >= 2] # Remove single occurrences
+testQuadgramsCount = testQuadgramsCount[testQuadgramsCount >= 2] # Remove single occurrences
 testQuadgramsGram = names(testQuadgramsCount)
 testQuadgramsLowerGram = sapply(testQuadgramsGram, function(gram) { getFirstNTokensFast(gram, 3)})
 testQuadgramsWordToPredict = sapply(testQuadgramsGram, function(gram) { getLastToken(gram) })
 testQuadgramsDT = data.table(gram = testQuadgramsGram, 
                              lowergram = testQuadgramsLowerGram, 
                              word_to_predict = testQuadgramsWordToPredict)
-testQuadgramsDTPathName = paste0(g_corpus_directory_en, '/test_quadgrams_DT_partition_', sprintf('%.1f', 100-partitionRate), '.partition_', sprintf('%.1f', sampleRate), '.rds')
+testQuadgramsDTPathName = paste0(g_corpus_directory_en, '/test_quadgrams_DT_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
 saveRDS(object = testQuadgramsDT, file = testQuadgramsDTPathName)
-
-# Read the test data table
-testQuadgramsDT = readRDS(file = testQuadgramsDTPathName)
+# testQuadgramsDT = readRDS(file = testQuadgramsDTPathName)
+dim(testQuadgramsDT)
 
 # Quick test the model
-predictions = predictNextNBestWords(model = modelUnderTest, type = 'MLE', sentence = 'Hello how are you doing today', matches_count_max = 5)
-predictions = predictNextNBestWords(model = modelUnderTest, type = 'MLE', sentence = 'djsh dsjhd sjhd  sdh sdj sjdh jsdh sdhjshdsjdh', matches_count_max = 5)
+set.seed(10)
+testSample = sample_n(testQuadgramsDT, 1)
+predictWithStupidBackoff2(model = modelUnderTest, sentence = testSample$lowergram, matches_count_max = 10)
 
+# Test the model's performance against the test quadgrams
+execTime <- system.time({
+    set.seed(10)
+    testSize = 100000
+    remove(testData)
+    testData = sample_n(testQuadgramsDT, testSize)
+    testCount = dim(testData)[1]
+    testResults = mapply(function(lowergram, word_to_predict) {
+        word_to_predict %in% predictWithStupidBackoff2(model = modelUnderTest,
+                                                       sentence = lowergram)$`Predicted Word`
+    },
+    testData$lowergram,
+    testData$word_to_predict)
+    predictionmatchCount = sum(testResults)
+    message(paste('Prediction match count   :', predictionmatchCount, 'out of', testCount, '(', round(predictionmatchCount*100/testCount, 2), '%)'))
+})
+execTimeSeconds = execTime["elapsed"]
+execTimeMins = execTimeSeconds/60
+execTimeSecsAfterMins = execTimeSeconds %% 60
+message(paste('Test executed in', round(execTimeMins), 'mins, ', round(execTimeSecsAfterMins, 2), 'secs'))
+
+# -----------------------------------------------------------------------------
+# create the test pentagrams
+# ~ 78 % success
+# -----------------------------------------------------------------------------
+tokenizer5 = function(x) NGramTokenizer(x, Weka_control(min = 5, max = 5))
+testPentagramsDtmPathName = paste0(g_corpus_directory_en, '/test_pentagrams_dtm_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
+execTime = system.time({
+    testPentagramsDtm = DocumentTermMatrix(cTest, control = list(tokenize = tokenizer5))
+    saveRDS(object = testPentagramsDtm, file = testPentagramsDtmPathName)
+})
+message(paste('DTM built in', round(execTime["elapsed"], 2), "secs"))
+# testPentagramsDtm = readRDS(file = testPentagramsDtmPathName)
+testPentagramsCount = colSums(as.matrix(testPentagramsDtm))
+testPentagramsCount = testPentagramsCount[testPentagramsCount >= 2] # Remove single occurrences
+testPentagramsGram = names(testPentagramsCount)
+testPentagramsLowerGram = sapply(testPentagramsGram, function(gram) { getFirstNTokensFast(gram, 4)})
+testPentagramsWordToPredict = sapply(testPentagramsGram, function(gram) { getLastToken(gram) })
+testPentagramsDT = data.table(gram = testPentagramsGram, 
+                             lowergram = testPentagramsLowerGram, 
+                             word_to_predict = testPentagramsWordToPredict)
+testPentagramsDTPathName = paste0(g_corpus_directory_en, '/test_pentagrams_DT_partition_', sprintf('%.1f', 100-partitionRate), '_sample_', sprintf('%.1f', sampleRate), '.rds')
+saveRDS(object = testPentagramsDT, file = testPentagramsDTPathName)
+# testPentagramsDT = readRDS(file = testPentagramsDTPathName)
+dim(testPentagramsDT)
+
+# Quick test the model
+set.seed(10)
+testSample = sample_n(testPentagramsDT, 1)
+predictWithStupidBackoff2(model = modelUnderTest, sentence = testSample$lowergram, matches_count_max = 10)
+
+# Test the model's performance against the test quadgrams
+execTime <- system.time({
+    set.seed(10)
+    remove(testData)
+    testData = testPentagramsDT
+    testCount = dim(testData)[1]
+    testResults = mapply(function(lowergram, word_to_predict) {
+        word_to_predict %in% predictWithStupidBackoff2(model = modelUnderTest,
+                                                       sentence = lowergram)$`Predicted Word`
+    },
+    testData$lowergram,
+    testData$word_to_predict)
+    predictionmatchCount = sum(testResults)
+    message(paste('Prediction match count   :', predictionmatchCount, 'out of', testCount, '(', round(predictionmatchCount*100/testCount, 2), '%)'))
+})
+execTimeSeconds = execTime["elapsed"]
+execTimeMins = execTimeSeconds/60
+execTimeSecsAfterMins = execTimeSeconds %% 60
+message(paste('Test executed in', round(execTimeMins), 'mins, ', round(execTimeSecsAfterMins, 2), 'secs'))
+
+
+# -----------------------------------------------------------------------------
+# Mixed N-Grams Test
+# -----------------------------------------------------------------------------
+execTime <- system.time({
+    set.seed(10)
+    remove(testData)
+    testData = rbind(
+        sample_n(testBigramsDT, 25000),
+        sample_n(testTrigramsDT, 25000),
+        sample_n(testQuadgramsDT, 25000),
+        sample_n(testPentagramsDT, 25000)
+    )
+    testCount = dim(testData)[1]
+    testResults = mapply(function(lowergram, word_to_predict) {
+        word_to_predict %in% predictWithStupidBackoff2(model = modelUnderTest,
+                                                       sentence = lowergram)$`Predicted Word`
+    },
+    testData$lowergram,
+    testData$word_to_predict)
+    predictionmatchCount = sum(testResults)
+    message(paste('Prediction match count   :', predictionmatchCount, 'out of', testCount, '(', round(predictionmatchCount*100/testCount, 2), '%)'))
+})
+execTimeSeconds = execTime["elapsed"]
+execTimeMins = execTimeSeconds/60
+execTimeSecsAfterMins = execTimeSeconds %% 60
+message(paste('Test executed in', round(execTimeMins), 'mins, ', round(execTimeSecsAfterMins, 2), 'secs'))
+
+
+# ------------------------------------ Old experimentations -------------------
 # Test!
 # Training data: 10% partition
 # Probability mode: MLE
