2797bb2a7f3db24179e183e98d55aa5621b8e713
Bertrand Rigaldies <brigaldies@gmail.com>
2016-10-06 20:28:28 -0400

    Code cleanup (Cont'd)

--- ngram.R
+++
@@ -1,7 +0,0 @@
-# ngram analysis with the ngram package
-
-sampleRate = 1
-corpusFilesPattern = paste0('^en_US(.)*sample_', sampleRate, '$')
-dirSamples = DirSource(directory = corpus_directory_en, pattern = corpusFilesPattern)
-
-# Load the text,
